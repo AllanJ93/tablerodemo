6530cf81c20ff1badff65e165ba0eda13c1d758b
Allan <al.josue93@gmail.com>
2023-10-03 00:07:27 -0600

    Trabajo de la noche

--- R/mod_operaciones.R
+++ R/mod_operaciones.R
@@ -4,28 +4,44 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_operaciones_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    # fluidPage(
+    #   shinyjs::useShinyjs(),
+    #   titlePanel("Reservaciones"),
+    #   # sidebarLayout(
+    #   #   sidebarPanel(
+    #   #     # Inputs excluded for brevity
+    #   #   ),
+    #   mainPanel(
+    #     tabsetPanel(
+    #       tabPanel("Datos (primer grupo)",
+    #                plotOutput("plot")),
+    #       tabPanel("Datos (segundo grupo)", verbatimTextOutput("summary")),
+    #       tabPanel("Datos (tercer grupo)", tableOutput("table"))
+    #     )
+    #   )
+    #   # )
+    # )
   )
 }
-    
+
 #' operaciones Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_operaciones_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_operaciones_ui("operaciones_1")
-    
+
 ## To be copied in the server
 # mod_operaciones_server("operaciones_1")
--- R/mod_reservaciones.R
+++ R/mod_reservaciones.R
@@ -4,28 +4,102 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @import shinycssloaders
+#' @importFrom shiny NS tagList
 mod_reservaciones_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    fluidPage(
+      titlePanel("Reservaciones"),
+      # sidebarLayout(
+      #   sidebarPanel(
+      #     # Inputs excluded for brevity
+      #   ),
+      mainPanel(
+        tabsetPanel(
+          tabPanel(
+            title = "Datos (primer grupo)",
+            hr(),
+            fluidRow(
+              column(
+                width = 3,
+                textInput("nombre_entrada", label = h3("Nombre"), value = ""),
+                hr(),
+                verbatimTextOutput("nombre_salida"),
+                textInput("agencia_entrada", label = h3("Agencia"), value = ""),
+                hr(),
+                verbatimTextOutput("agencia_salida"),
+              ),
+              column(
+                width = 3,
+                textInput("apellidoPa_entrada", label = h3("Apellido Paterno"), value = ""),
+                hr(),
+                verbatimTextOutput("apellidoPa_salida"),
+                textInput("agente_entrada", label = h3("Agente"), value = ""),
+                hr(),
+                verbatimTextOutput("agente_salida"),
+              ),
+              column(
+                width = 3,
+                textInput("apellidoMa_entrada", label = h3("Apellido Materno"), value = ""),
+                hr(),
+                verbatimTextOutput("apellidoMa_salida")
+              ),
+            )
+          ),
+          tabPanel("Datos (segundo grupo)", verbatimTextOutput("summary")),
+          tabPanel("Datos (tercer grupo)", tableOutput("table"))
+        )
+      )
+      # )
+    )
   )
 }
-    
+
 #' reservaciones Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_reservaciones_server <- function(id){
-  moduleServer( id, function(input, output, session){
+  moduleServer(id, function(input, output, session){
     ns <- session$ns
- 
+
+    function(input, output) {
+
+      output$nombre_salida <- renderPrint({input$nombre_entrada})
+
+    }
+
+    function(input, output) {
+
+      output$apellidoPa_salida <- renderPrint({input$apellidoPa_entrada})
+
+    }
+
+    function(input, output) {
+
+      output$apellidoMa_salida <- renderPrint({input$apellidoMa_entrada})
+
+    }
+
+    function(input, output) {
+
+      output$agencia_salida <- renderPrint({input$agencia_entrada})
+
+    }
+
+    function(input, output) {
+
+      output$agente_salida <- renderPrint({input$agente_entrada})
+
+    }
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_reservaciones_ui("reservaciones_1")
-    
+
 ## To be copied in the server
 # mod_reservaciones_server("reservaciones_1")
